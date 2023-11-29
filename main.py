9ba8b4eaccdcaa2e7ea7df12da27f297f604ffbf
Maser <Maser10@mail.ru>
2023-11-29 16:59:04 +0500

    change

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-def bubble_sort(arr):
+def bubble_sort(arr, ascending=True):
     n = len(arr)
 
     for i in range(n - 1):
         for j in range(0, n - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-
+            if ascending:
+                if arr[j] > arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            else:
+                if arr[j] < arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 n = int(input("Введите количество чисел: "))
 numbers = []
 for _ in range(n):
     num = float(input("Введите число: "))
     numbers.append(num)
-bubble_sort(numbers)
+direction = input("Выберите направление сортировки (asc/desc): ").lower()
+bubble_sort(numbers, ascending=direction == "asc")
 print("Отсортированный список:", numbers)
